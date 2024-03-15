07c68e73a378dc0bf5ccb98271ced9aac9db5f4f
rodmasalskiene <rodolfowm707@gmail.com>
2024-03-15 14:34:01 -0300

    successful request/response using HttpClient

--- CoolestAPI/Models/User.cs
+++ CoolestAPI/Models/User.cs
@@ -8,28 +8,32 @@ namespace CoolestAPI.Models
 {
     public class User
     {
-        private int id;
-        private string name = "";
-        private string email = "";
-        public int Id
-        {
-            get { return id; }
-            set { id = value; }
-        }
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
-        public string Email
-        {
-            get { return email; }
-            set { email = value; }
-        }
+        public int id { get; set; }
+        public string? name { get; set;}
+        public string? email { get; set; }
 
-        public User()
-        {
+        //private int id;
+        //private string name = "";
+        //private string email = "";
+        //public int Id
+        //{
+        //    get { return id; }
+        //    set { id = value; }
+        //}
+        //public string Name
+        //{
+        //    get { return name; }
+        //    set { name = value; }
+        //}
+        //public string Email
+        //{
+        //    get { return email; }
+        //    set { email = value; }
+        //}
 
-        }
+        //public User()
+        //{
+
+        //}
     }
 }
